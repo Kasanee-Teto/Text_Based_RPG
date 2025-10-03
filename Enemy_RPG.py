b6c4fe56bb9d04ce54bb9efe95d9cd86d27fae87
Kasanee-Teto <fortinolouis62@gmail.com>
2025-10-03 10:39:09 +0700

    Update Enemy_RPG.py

--- Enemy_RPG.py
+++ Enemy_RPG.py
@@ -1,19 +1,40 @@
-class Enemy_RPG :
-    def __init__(self, name, hp, attack, defense):
-        self.name = name
-        self.hp = hp
-        self.attack_power = attack
-        self.defense = defense
+from Character_RPG import character
 
-    def is_alive(self):
-        return self.hp > 0
 
-    def take_damage(self, damage):
-        self.hp -= damage
-        if self.hp < 0:
-            self.hp = 0
-
+# --- SUBCLASS ENEMY SPESIAL ---
+class Goblin(Enemy):
     def attack(self, target):
-        damage = max(0, self.attack_power - target.defense)
-        target.take_damage(damage)
-        print(f"{self.name} menyerang {target.name} dan menyebabkan {damage} damage!")
+        super().attack(target)
+
+class Wolf(Enemy):
+    def attack(self, target):
+        super().attack(target)
+        target.status_effects.append("bleeding")
+        print(f"{target.name} terkena bleeding!")
+
+
+class Ogre(Enemy):
+    def attack(self, target):
+        super().attack(target)
+        target.status_effects.append("weakened")
+        print(f"{target.name} stat turun sementara (weakened)!")
+
+
+class Vampire(Enemy):
+    def attack(self, target):
+        super().attack(target)
+        heal = int(self.attack_power * 0.3)
+        self.hp += heal
+        print(f"{self.name} menyedot darah! HP pulih {heal}.")
+
+
+class Demon(Enemy):
+    def attack(self, target):
+        super().attack(target)
+        target.status_effects.append("bleeding")
+        print(f"{target.name} terkena bleeding!")
+        target.status_effects.append("weakened")
+        print(f"{target.name} stat turun sementara (weakened)!")
+        heal = int(self.attack_power * 0.3)
+        self.hp += heal
+        print(f"{self.name} menyedot d arah! HP pulih {heal}.")
