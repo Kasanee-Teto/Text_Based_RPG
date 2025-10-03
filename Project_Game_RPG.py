2e90b39fcc151ce48d252278cae531a378a8c818
Kasanee-Teto <fortinolouis62@gmail.com>
2025-10-03 10:38:09 +0700

    Update Project_Game_RPG.py

--- Project_Game_RPG.py
+++ Project_Game_RPG.py
@@ -32,45 +32,6 @@ class Enemy(Character):
         self.defeated += 1
         print(f"{self.name} telah dikalahkan {self.defeated} kali, jadi lebih kuat!")
 
-
-# --- SUBCLASS ENEMY SPESIAL ---
-class Goblin(Enemy):
-    def attack(self, target):
-        super().attack(target)
-
-class Wolf(Enemy):
-    def attack(self, target):
-        super().attack(target)
-        target.status_effects.append("bleeding")
-        print(f"{target.name} terkena bleeding!")
-
-
-class Ogre(Enemy):
-    def attack(self, target):
-        super().attack(target)
-        target.status_effects.append("weakened")
-        print(f"{target.name} stat turun sementara (weakened)!")
-
-
-class Vampire(Enemy):
-    def attack(self, target):
-        super().attack(target)
-        heal = int(self.attack_power * 0.3)
-        self.hp += heal
-        print(f"{self.name} menyedot darah! HP pulih {heal}.")
-
-
-class Demon(Enemy):
-    def attack(self, target):
-        super().attack(target)
-        target.status_effects.append("bleeding")
-        print(f"{target.name} terkena bleeding!")
-        target.status_effects.append("weakened")
-        print(f"{target.name} stat turun sementara (weakened)!")
-        heal = int(self.attack_power * 0.3)
-        self.hp += heal
-        print(f"{self.name} menyedot d arah! HP pulih {heal}.")
-
 #Menu
 def menu():
     print("=== MENU RPG SEDERHANA ===")
@@ -97,3 +58,4 @@ def main():
 #Program
 if __name__ == "__main__":
     main()
+
