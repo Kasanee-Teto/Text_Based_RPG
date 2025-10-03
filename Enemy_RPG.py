30a9cf9be4bb197eb37d98c679cc68d8dab232a7
Kasanee-Teto <fortinolouis62@gmail.com>
2025-10-03 10:40:21 +0700

    Update Enemy_RPG.py

--- Enemy_RPG.py
+++ Enemy_RPG.py
@@ -1,6 +1,22 @@
 from Character_RPG import character
 
+class Enemy(Character):
+    def __init__(self, name, hp, attack, defense, exp_reward):
+        super().__init__(name, hp, attack, defense)
+        self.exp_reward = exp_reward
+        self.defeated = 0
 
+    def scale_difficulty(self):
+        factor = 1 + (0.2 * self.defeated)
+        scaled_hp = int(self.hp * factor)
+        scaled_attack = int(self.attack_power * factor)
+        scaled_defense = int(self.defense * factor)
+        return {"hp": scaled_hp, "attack": scaled_attack, "defense": scaled_defense}
+
+    def mark_defeated(self):
+        self.defeated += 1
+        print(f"{self.name} telah dikalahkan {self.defeated} kali, jadi lebih kuat!")
+        
 # --- SUBCLASS ENEMY SPESIAL ---
 class Goblin(Enemy):
     def attack(self, target):
@@ -38,3 +54,4 @@ class Demon(Enemy):
         heal = int(self.attack_power * 0.3)
         self.hp += heal
         print(f"{self.name} menyedot d arah! HP pulih {heal}.")
+
